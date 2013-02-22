28ec48a21cdaaca3138550704e6e0cf468796867
Philipp Lutz <atpunkt@punktat.de>
2013-02-22 17:44:24 +0100

    major restructuring of client code

--- Readme.md
+++ Readme.md
@@ -2,3 +2,12 @@
 
 Ein Html5-basiertes Zeichenprogramm mit Ajax-Speicherung der Ergebnisse und
 der Möglichkeit, Hintergrundgrafiken einzubinden.
+
+## Todos
+
+* Multiplay-synchronisation
+* Image-Upload by Dragdrop
+* Store image information with Drawing
+* Selectable Backgrounds
+* Thumbnails
+* Undo, Delete pen
--- public/javascripts/wolken.js
+++ public/javascripts/wolken.js
@@ -1,148 +1,169 @@
 "use strict";
 var app = {};
+var ajax = {
+    ajaxurl: window.location.protocol + "//" + window.location.host + "/ajax/",
+    loadData: function(id) {
+        var loadData = {
+            session_id: id
+        };
+        var loadString = JSON.stringify(loadData);
+        $.ajax(this.ajaxurl + "load",
+                {
+                    data: {data: loadString},
+                    success: app.load_success
+                });
+    },
+    saveData: function(data) {
+        $.ajax(this.ajaxurl + "save",
+                {
+                    data: {data: data},
+                    success: app.save_success
+                })
+    }
+}
 var wolke = function() {
-    this.canvas = 0;
     this.clickX = [];
     this.clickY = [];
     this.currentX = [];
     this.currentY = [];
     this.paint = false;
     this.clickDrag = [];
-    this.context = 0
-};
-app = {
-    masterurl: window.location.protocol + "//" + window.location.host + "/",
-    ajaxurl: window.location.protocol + "//" + window.location.host + "/ajax/",
-    runit: function() {
-        app.queueSave(true);
-        app.finishCurrent();
-        var saveData = {
-            clickX: app.states.clickX,
-            clickY: app.states.clickY
-        };
-        if (app.states.session_id)
-            saveData.session_id = app.states.session_id;
+    this.setSessionID = function(sesid) {
+        app.wolken.session_id = sesid;
+    };
+    this.getSaveData = function() {
 
-        var saveString = JSON.stringify(saveData);
-        //$("#messageBox").load(app.masterurl + "save", {data: saveString});
-        $.ajax(app.ajaxurl + "save",
-                {
-                    data: {data: saveString},
-                    success: app.save_success
-                });
-    },
-    save_success: function(data, status, xhr) {
-        $("#messageBox").html(data.Message);
-        app.states.session_id = data.session_id;
-        var imgUrl = app.masterurl + "i/" + data.session_id;
-        var testurl = /\/i\/.{16}$/;
-        if (!testurl.test(document.location.href))
-        {
-            var newUrl = "/i/" + data.session_id;
-            if (window.history.pushState) {
-                window.history.pushState(null, null, newUrl);
-            }
-            else {
-                window.location.href = imgUrl;
-            }
+        var saveData = {
+            clickX: this.clickX,
+            clickY: this.clickY
+        };
+        if (this.session_id)
+            saveData.session_id = this.session_id;
+
+        return JSON.stringify(saveData);
+    };
+    this.loadData = function(data) {
+        this.clickX = data.linedata.clickX;
+        this.clickY = data.linedata.clickY;
+        this.session_id = data.session_id;
+    };
+    this.pushXY = function(x, y) {
+        this.currentX.push(x);
+        this.currentY.push(y);
+    };
+    this.finishCurrent = function() {
+        if (this.currentX.length > 0) {
+            this.clickX.push(this.currentX);
+            this.clickY.push(this.currentY);
         }
-        ;
-        $("#topMessage").html($("<a/>").attr('href', imgUrl).html(imgUrl)).prepend("<span>ShareLink:</span> ");
-    },
-    load_success: function(data, status, xhr) {
-        $("#messageBox").html(data.Message);
-        app.states.clickX = data.linedata.clickX;
-        app.states.clickY = data.linedata.clickY;
-        app.states.session_id = data.session_id;
-        app.redraw();
-    },
-    states: new wolke(),
-    redraw_path_fragment: function(ctx, drawX, drawY) {
-        ctx.beginPath();
-        ctx.moveTo(drawX[drawX.length - 1], drawY[drawX.length - 1]);
-        for (var j = drawX.length - 1; j > 0; j--) {
-            ctx.lineTo(drawX[j], drawY[j]);
-        }
-        ctx.stroke();
-    },
-    redraw_current: function(ctx) {
-        var currentLength = app.states.currentX.length;
-        if (currentLength > 1) {
-            ctx.beginPath();
-            ctx.moveTo(app.states.currentX[currentLength - 1], app.states.currentY[currentLength - 1]);
-            for (var i = currentLength - 1; i > 0; i--)
-                ctx.lineTo(app.states.currentX[i], app.states.currentY[i]);
-            ctx.stroke();
-        }
-    },
-    get_context: function() {
-        var ctx = app.states.context;
+        this.currentX = [];
+        this.currentY = [];
+    };
+    this.redraw = function(drawing) {
+        var ctx = drawing.get_context();
+        for (var i = 0; i < this.clickX.length; i++)
+            drawing.redraw_path_fragment(ctx, {X: this.clickX[i], Y: this.clickY[i]});
+        drawing.redraw_current(ctx, {X: this.currentX, Y: this.currentY});
+    };
+};
+var drawing = function() {
+    this.canvas = $("#canvas");
+    this.context = this.canvas[0].getContext("2d");
+    this.get_context = function() {
+        var ctx = this.context;
         ctx.strokeStyle = "#000";
         ctx.lineJoin = "round";
         ctx.lineWidth = 2;
         return ctx;
+    };
+    this.redraw_path_fragment = function(ctx, frag) {
+        ctx.beginPath();
+        ctx.moveTo(frag.X[frag.X.length - 1], frag.Y[frag.X.length - 1]);
+        for (var j = frag.X.length - 1; j > 0; j--) {
+            ctx.lineTo(frag.X[j], frag.Y[j]);
+        }
+        ctx.stroke();
+    };
+    this.redraw_current = function(ctx, cFrag) {
+        var currentLength = cFrag.X.length;
+        if (currentLength > 1) {
+            ctx.beginPath();
+            ctx.moveTo(cFrag.X[currentLength - 1], cFrag.Y[currentLength - 1]);
+            for (var i = currentLength - 1; i > 0; i--)
+                ctx.lineTo(cFrag.X[i], cFrag.Y[i]);
+            ctx.stroke();
+        }
+    };
+    this.newImg = function(image) {
+        this.canvas.attr({Width: image.width + "px", Height: image.height + "px"}).attr("style", "Border:3px solid black");
+
+        this.context.drawImage(image, 0, 0, image.width, image.height);
+    };
+    this.loadImage = function(imageUrl) {
+        this.cloudImg = $(new Image()).attr("id", "loadImg");
+        this.cloudImg.on("load", app.runners.image_loaded)
+                .attr("src", imageUrl);
+    };
+};
+
+app = {
+    masterurl: window.location.protocol + "//" + window.location.host + "/",
+    wolken: new wolke(),
+    drawing: new drawing(),
+    saveImage: function() {
+        app.queueSave(true);
+        app.wolken.finishCurrent();
+        ajax.saveData(app.wolken.getSaveData());
+    },
+    setBrowserUrl: function(newid) {
+        var testurl = /\/i\/.{16}$/;
+        if (!testurl.test(document.location.href))
+            if (window.history.pushState)
+                window.history.pushState(null, null, "/i/" + newid);
+            else
+                window.location.href = imgUrl;
+    },
+    setTopMessage: function(message) {
+        $("#topMessage").html(message);
+    },
+    save_success: function(data, status, xhr) {
+        $("#messageBox").html(data.Message);
+        
+        app.wolken.setSessionID(data.session_id);
+        app.setBrowserUrl(data.session_id);
+        var imgUrl = app.masterurl + "i/" + data.session_id;
+        app.setTopMessage($("<span>ShareLink:</span> ").append($("<a/>").attr('href', imgUrl).html(imgUrl)));
+    },
+    load_success: function(data, status, xhr) {
+        $("#messageBox").html(data.Message);
+        app.wolken.loadData(data);
+        app.redraw();
     },
     redraw: function() {
-        var ctx = app.get_context();
-        for (var i = 0; i < app.states.clickX.length; i++)
-            app.redraw_path_fragment(ctx, app.states.clickX[i], app.states.clickY[i]);
-
-        app.redraw_current(ctx);
-    },
-    finishCurrent: function() {
-        if (app.states.currentX.length > 0) {
-            app.states.clickX.push(app.states.currentX);
-            app.states.clickY.push(app.states.currentY);
-        }
-        app.states.currentX = [];
-        app.states.currentY = [];
+        app.wolken.redraw(app.drawing);
     },
     queueSave: function(clear) {
-        if (app.states.currentTimeout) {
-            clearTimeout(app.states.currentTimeout);
+        if (app.currentTimeout) {
+            clearTimeout(app.currentTimeout);
         }
-        app.states.currentTimeout = (clear) ? null : setTimeout(app.runit, 1000);
+        app.currentTimeout = (clear) ? null : setTimeout(app.saveImage, 1000);
     },
     addClick: function(x, y, dragging)
     {
-        if (dragging) {
-            app.states.currentX.push(x);
-            app.states.currentY.push(y);
-        }
-        else {
-            app.finishCurrent();
-
-        }
+        dragging ? app.wolken.pushXY(x, y) : app.wolken.finishCurrent();
         app.queueSave();
     },
     runners: {
         install_events: function() {
             var event_element;
-            for (event_element in app.events) {
+            for (event_element in app.events)
                 $(event_element).on(app.events[event_element]);
-            }
-
         },
         image_loaded: function() {
-
-            app.states.canvas.attr({Width: this.width + "px", Height: this.height + "px"}).attr("style", "Border:3px solid black");
-            app.states.context = app.states.canvas[0].getContext("2d");
-            app.states.context.drawImage(this, 0, 0, this.width, this.height);
-            var session_id = $("#load_id").val();
-            if (session_id != "") {
-                var loadData = {
-                    session_id: session_id
-                };
-
-                var loadString = JSON.stringify(loadData);
-
-                $.ajax(app.ajaxurl + "load",
-                        {
-                            data: {data: loadString},
-                            success: app.load_success
-                        });
-            }
-            ;
+            app.drawing.newImg(this);
+            var session_id = $("#load_id").val()
+            if (session_id)
+                ajax.loadData(session_id);
             if (window.File && window.FileList && window.FileReader) {
                 app.runners.init_fileDrop();
             }
@@ -151,25 +172,17 @@ app = {
             $("#canvas").on({
                 dragover: app.handlers.dragover,
                 dragleave: app.handlers.dragleave,
-                drop: app.handlers.drop,
+                drop: app.handlers.drop
             });
         },
-        loadImage: function(imageUrl) {
-            app.states.loadImg = $(new Image()).attr("id", "loadImg");
-            app.states.loadImg.on("load", app.runners.image_loaded)
-                    .attr("src", imageUrl);
-
-        },
         setup: function() {
-            app.states.canvas = $("<canvas/>").attr("id", "canvas");
-            $('#mainimg').append(app.states.canvas);
             $.ajaxSetup({
                 dataType: "json",
                 type: 'POST'
             });
-            app.runners.loadImage('/images/wolken2.jpg');
-        }
 
+            app.drawing.loadImage('/images/wolken2.jpg');
+        }
     },
     handlers: {
         dragover: function() {
@@ -185,19 +198,19 @@ app = {
 };
 app.events = {
     "#runbutton": {
-        click: app.runit
+        click: app.saveImage
     },
     '#canvas': {
         mouseup: function(e) {
-            app.states.paint = false;
+            app.wolken.paint = false;
         }, mouseleave: function(e) {
-            app.states.paint = false;
+            app.wolken.paint = false;
         },
         mouseenter: function(e) {
-            app.states.paint = e.button & 1;
+            app.wolken.paint = e.button & 1;
         },
         mousemove: function(e) {
-            if (app.states.paint) {
+            if (app.wolken.paint) {
                 app.addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
                 app.redraw();
             }
@@ -205,7 +218,7 @@ app.events = {
         mousedown: function(e) {
             var mouseX = e.pageX - this.offsetLeft;
             var mouseY = e.pageY - this.offsetTop;
-            app.states.paint = true;
+            app.wolken.paint = true;
             app.addClick(mouseX, mouseY, false);
             app.redraw();
         }
